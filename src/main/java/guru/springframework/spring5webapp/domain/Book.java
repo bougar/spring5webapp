5739bcda5c593bb684296c79a8f6e989a05965cb
Your Name <you@example.com>
2021-01-21 22:32:30 +0100

    Add BookController
    A BookController has been added to the project. This controller
    will manage any operation over the books store on Database

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -2,7 +2,6 @@ package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -12,10 +11,15 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-    @ManyToMany(targetEntity = Author.class)
+
+    @ManyToOne
+    @JoinColumn(name = "publisher_id")
+    private Publisher publisher;
+
+    @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
-    private Set<Author> authors = new HashSet<Author>(0);
+    private Set<Author> authors = new HashSet<>();
 
     public Book() {
     }
@@ -23,6 +27,14 @@ public class Book {
     public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     public String getTitle() {
@@ -53,19 +65,8 @@ public class Book {
         this.id = id;
     }
 
-    @Id
     public Long getId() {
         return id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Book book = (Book) o;
-
-        return Objects.equals(id, book.id);
     }
 
     @Override
